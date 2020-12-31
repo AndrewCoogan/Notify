3f5e21f4026aff6d434bdc5c2f78cd4bb82f7da8
Drew Coogan <andrew.l.coogan@gmail.com>
2020-12-31 14:39:30 -0800

    Updated backend user model and seed file

--- script/seed.js
+++ script/seed.js
@@ -8,8 +8,18 @@ async function seed() {
   console.log('db synced!')
 
   const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
+    User.create({
+      firstName: 'Claire',
+      lastName: 'Brown',
+      email: 'claireconstans@msn.com',
+      password: '123'
+    }),
+    User.create({
+      firstName: 'Drew',
+      lastName: 'Coogan',
+      email: 'andrew.l.coogan@gmail.com',
+      password: '123'
+    })
   ])
 
   console.log(`seeded ${users.length} users`)
